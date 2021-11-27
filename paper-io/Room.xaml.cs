2bb53795bcec48bfc290f12593beb05f329552a8
Timur Puchinsky <timur.2.puchinsky@gmail.com>
2021-11-27 18:04:49 +0300

    1

--- paper-io/Room.xaml.cs
+++ paper-io/Room.xaml.cs
@@ -17,65 +17,96 @@ using System.Windows.Threading;
 namespace paper_io
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для Start.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Room : Window
     {
-        enum Direction { left, right, up, down, none };
+        /// <summary>
+        /// перечисление которое обрабатывает нажатия на клавиши и задает направление движения игроку
+        /// </summary>
+        enum Direction { left, right, up, down }
         double x = 0;
         double y = 0;
-        public MainWindow()
+        double x1 = 0;
+        double y1 = 0;
+        public Room()
         {
             InitializeComponent();
             DispatcherTimer timer = new DispatcherTimer();
             timer.Tick += new EventHandler(MovePlayer);
             timer.Start();
         }
+
+        /// <summary>
+        /// движение игрока (как будут меняться координаты игрока при нажатии)
+        /// </summary>
+        /// <param name="sender">
+        /// генерация нажатия на клавиши
+        /// </param>
+        /// <param name="e">
+        /// обработка нажатия на клавиши
+        /// </param>
         private void MovePlayer(object sender, EventArgs e)
         {
             if (Keyboard.IsKeyDown(Key.Left))
             {
-                x -= .05;
+                x -= .025;
                 Canvas.SetLeft(Player, x);
+                x1 -= .025;
+                UpdatePlayerPositionAndCamera();
             }
             if (Keyboard.IsKeyDown(Key.Up))
             {
-                y -= .05;
+                y -= .025;
                 Canvas.SetTop(Player, y);
+                y1 -= .025;
+                UpdatePlayerPositionAndCamera();
             }
             if (Keyboard.IsKeyDown(Key.Down))
             {
-                y += .05;
+                y += .025;
                 Canvas.SetTop(Player, y);
+                y1 += .025;
+                UpdatePlayerPositionAndCamera();
             }
             if (Keyboard.IsKeyDown(Key.Right))
             {
-                x += .05;
+                x += .025;
                 Canvas.SetLeft(Player, x);
+                x1 += .025;
+                UpdatePlayerPositionAndCamera();
             }
         }
-        //private void MoveCamera(object sender, EventArgs e)
-        //{
-        //    if (Keyboard.IsKeyDown(Key.Left))
-        //    {
-        //        x -= .05;
-        //        Canvas.SetLeft(CanvasViewer, x);
-        //    }
-        //    if (Keyboard.IsKeyDown(Key.Up))
-        //    {
-        //        y -= .05;
-        //        Canvas.SetTop(CanvasViewer, y);
-        //    }
-        //    if (Keyboard.IsKeyDown(Key.Down))
-        //    {
-        //        y += .05;
-        //        Canvas.SetTop(CanvasViewer, y);
-        //    }
-        //    if (Keyboard.IsKeyDown(Key.Right))
-        //    {
-        //        x += .05;
-        //        Canvas.SetLeft(CanvasViewer, x);
-        //    }
-        //}
+
+        /// <summary>
+        /// методы обновления позиции игрока и камеры
+        /// </summary>
+        private void UpdatePlayerPositionAndCamera()
+        {
+            UpdatePlayerPosition();
+            //UpdateCamera();
+        }
+
+        /// <summary>
+        /// обновление позиции игрока
+        /// </summary>
+        private void UpdatePlayerPosition()
+        {
+            int x2 = (int)x1;
+            int y2 = (int)y1;
+            //int[,] PlayerPosition = new int[x2, y2];
+            CanvasViewer.PointToScreen(new Point(x2, x2));
+            //View.Margin = new Thickness(1, 1, 1, 1);
+        }
+
+        /// <summary>
+        /// обновление позиции камеры 
+        /// </summary>
+        private void UpdateCamera()
+        {
+            int x2 = (int)x1;
+            int y2 = (int)y1;
+            int[,] PlayerPosition = new int[x2, y2];
+        }
     }
 }
